1b2761e3c5d6d39f46bdcab2d4882eb5073e368a
Srajal591 <143382062+Srajal591@users.noreply.github.com>
2025-08-26 23:52:08 +0530

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,37 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "class", // enables dark mode via class
-  content: [
-  "./index.html",
-  "./src/App.jsx",
-  "./src/main.jsx",
-  "./src/components/*.{js,jsx}",
-  "./src/pages/*.{js,jsx}",
-  "./src/store/*.{js,jsx}",
-  "./src/hooks/*.{js,jsx}",
-  "./src/utils/*.{js,jsx}",
-],
-  theme: {
-    extend: {
-      colors: {
-        background: {
-          light: "#f9f9f9",
-          dark: "#111827",
-        },
-        surface: {
-          light: "#ffffff",
-          dark: "#1f2937",
-        },
-        text: {
-          light: "#111827",
-          dark: "#f3f4f6",
-        },
-        accent: {
-          DEFAULT: "#8b5cf6", // muted violet for modern accent
-          hover: "#7c3aed",
-        },
-      },
-    },
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   darkMode: "class", // enables dark mode via class
+//   content: [
+//   "./index.html",
+//   "./src/App.jsx",
+//   "./src/main.jsx",
+//   "./src/components/*.{js,jsx}",
+//   "./src/pages/*.{js,jsx}",
+//   "./src/store/*.{js,jsx}",
+//   "./src/hooks/*.{js,jsx}",
+//   "./src/utils/*.{js,jsx}",
+// ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: {
+//           light: "#f9f9f9",
+//           dark: "#111827",
+//         },
+//         surface: {
+//           light: "#ffffff",
+//           dark: "#1f2937",
+//         },
+//         text: {
+//           light: "#111827",
+//           dark: "#f3f4f6",
+//         },
+//         accent: {
+//           DEFAULT: "#8b5cf6", // muted violet for modern accent
+//           hover: "#7c3aed",
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
